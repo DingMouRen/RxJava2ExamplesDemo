7c4a2cd2ac2ada05ecafa1eca7e312feb0c3162c
cuiqingdong@qupeiyin.com <cuiqingdong@qupeiyin.com>
2018-12-21 11:07:11 +0800

    背压

--- ExamplesForAndroid/app/src/main/java/com/dingmouren/examplesforandroid/ui/operators/operator/FlowableActivity.java
+++ ExamplesForAndroid/app/src/main/java/com/dingmouren/examplesforandroid/ui/operators/operator/FlowableActivity.java
@@ -3,9 +3,25 @@ package com.dingmouren.examplesforandroid.ui.operators.operator;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.dingmouren.examplesforandroid.base.BaseOperatorActivity;
 import com.dingmouren.examplesforandroid.model.OperatorModel;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
+import io.reactivex.FlowableEmitter;
+import io.reactivex.FlowableOnSubscribe;
+import io.reactivex.FlowableSubscriber;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * @author dingmouren
@@ -41,6 +57,118 @@ public class FlowableActivity extends BaseOperatorActivity {
     protected void test() {
         mTvLog.append("\n\n");
 
+//        showBackpressure();
+//        showErrorStrategy();
+        showFlowableCrrect();
 
+    }
+
+    private void showFlowableCrrect(){
+        Flowable.create(new FlowableOnSubscribe<Integer>() {
+                    @Override
+                    public void subscribe(FlowableEmitter<Integer> e) throws Exception {
+                        int i = 0;
+                        while (true) {
+                            if (e.requested() == 0) continue;//此处添加代码，让flowable按需发送数据
+                            Log.e("Flowable","发射 --->"+i);
+                            i++;
+                            e.onNext(i);
+                        }
+                    }
+                }, BackpressureStrategy.MISSING)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(Schedulers.newThread())
+                .subscribe(new Subscriber<Integer>() {
+                    private Subscription mSubscription;
+
+                    @Override
+                    public void onSubscribe(Subscription s) {
+                        s.request(1);            //设置初始请求数据量为1
+                        mSubscription = s;
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        try {
+                            Thread.sleep(50);
+                            Log.e("Flowable","接收到 --->"+integer);
+                            mSubscription.request(1);//每接收到一条数据增加一条请求量
+                        } catch (InterruptedException ignore) {
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                    }
+
+                    @Override
+                    public void onComplete() {
+                    }
+                });
+
+    }
+
+
+    private void showErrorStrategy(){
+        Flowable
+                .create(new FlowableOnSubscribe<Integer>() {
+                    @Override
+                    public void subscribe(FlowableEmitter<Integer> e) throws Exception {
+                        for (int i = 1; i < 130; i++) {
+                            System.out.println("发射---->" + i);
+                            e.onNext(i);
+                        }
+                        e.onComplete();
+                    }
+                }, BackpressureStrategy.ERROR)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(Schedulers.newThread())
+                .subscribe(new Subscriber<Integer>() {
+                    @Override
+                    public void onSubscribe(Subscription s) {
+                        s.request(1);//设置下游的请求量为1，Subscription:它用于表示对数据的需求和取消需求（并允许资源清理）
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        System.out.println("接收------>" + integer);
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                        t.printStackTrace();
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("接收------>完成");
+                    }
+                });
+
+    }
+
+
+    /**
+     * 展示背压
+     */
+    private void showBackpressure(){
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                int i = 0;
+                while (true){
+                    i++;
+                    e.onNext(i);
+                }
+            }
+        }).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        Thread.sleep(5000);
+                        Log.e("accept","接收到 " + integer);
+                    }
+                });
     }
 }
